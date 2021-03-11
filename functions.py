d5d33de8c445f83fe7725ea14c0be5d5524aae1c
KristjanVeensalu <kristjan.veensalu@gmail.com>
2021-03-11 17:16:31 +0200

    Prototype

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import subprocess,sys,json,os,time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 def install(package):
     reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
@@ -8,3 +10,20 @@ def install(package):
     else:
         import pip
         os.system("pip install --user "+ package)
+
+def login(user, password):
+    driver = webdriver.Chrome()
+    driver.get('https://www.messenger.com/')
+    clickme = driver.find_element_by_xpath('//*[contains(@id,"u_0_g")]')
+    clickme.click()
+    time.sleep(2)
+    clickme = driver.find_element_by_xpath('//*[contains(@id,"email")]')
+    clickme.click()
+    clickme.send_keys(user)
+    clickme = driver.find_element_by_xpath('//*[contains(@id,"pass")]')
+    clickme.send_keys(password)
+    clickme = driver.find_element_by_xpath('//*[contains(@id,"loginbutton")]')
+    clickme.click()
+    driver.get("https://www.messenger.com/t/3346267432111017/")
+    time.sleep(3)
+    return driver
--- main.py
+++ main.py
@@ -1,11 +1,62 @@
 import functions as fc
-
+import time
 #Software check, install.
 fc.install("requests")
+fc.install("selenium")
+fc.install("pynput")
+fc.install("keyboard")
 
 #Import dependancies
 import requests
+import keyboard
+from selenium import webdriver
 from requests.auth import HTTPBasicAuth 
+from selenium.webdriver.common.keys import Keys
+
+wordsToDetect = ['Millal session?','next session when','millal sess?','session when?']
+running = True
+
+username = str(input("Messenger username? - "))
+password = str(input("Messenger password? - "))
+
+driver = fc.login(username,password)
+
+#keyboard.press_and_release("enter")
+
+print(type(driver.page_source))
+time.sleep(5)
+textAlreadyRespondedto = []
+incomingMessages = driver.find_elements_by_class_name("oo9gr5id")
+outgoingMessages = driver.find_elements_by_class_name("ljqsnud1")
 
 
-print("Hello world!")
+while running:
+    incomingMessages = driver.find_elements_by_class_name("oo9gr5id")
+    outgoingMessages = driver.find_elements_by_class_name("ljqsnud1")
+    latestOutMessage = outgoingMessages[-1].get_attribute('innerText')
+    latestInMessage = incomingMessages[-1].get_attribute('innerText')
+    for x in wordsToDetect:
+        if x in latestInMessage or x in latestOutMessage: 
+            keyboard.write("Järgmine reede on plaanis")
+            keyboard.press_and_release("enter")
+            time.sleep(2)
+    time.sleep(10)
+
+
+'''
+    for x in wordsToDetect:
+        try:
+            if className not in elementsAlreadyResponded:
+                elementsAlreadyResponded.append(className)
+                print(elementsAlreadyResponded)
+                if x == 'Millal session?':
+                        keyboard.write("Found '" + x + "' in chat.")
+                        keyboard.press_and_release("enter")
+                        keyboard.write("Järgmine reede on plaanis")
+                        keyboard.press_and_release("enter")
+                        time.sleep(20)
+        except:
+            #keyboard.write("Found '" + x + "' in chat.")
+            #keyboard.press_and_release("enter")
+            print("Listening for text")
+            time.sleep(10)'''
